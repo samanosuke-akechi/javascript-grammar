53851d6887a37165dcec14979ea8950881c9a534
samanosuke-akechi <78411965+samanosuke-akechi@users.noreply.github.com>
2021-04-22 23:19:04 +0900

    Initial commit

---
+++ practice.js
@@ -0,0 +1,17 @@
+// console.log();
+  // ブラウザのコンソールにテキストを表示させるメソッド
+console.log("Hello World!!");  // => Hello World!!
+
+// 変数の定義 let
+let fruit = "apple"  // 変数fruitに"apple"を定義
+console.log(fruit);  // => apple
+  // 変数の再代入
+fruit = "orange"  // 変数fruitに"orange"を再代入
+console.log(fruit);  // => orange
+    //再代入の時にlet fruit = "orange"とするとエラーが起こる（再定義不可）
+
+// 定数 const
+const sport = "soccer"  //定数sportに"soccer"を定義
+console.log(sport);
+  // 定数は再代入・再定義は出来ない
+  // const sport = "baseball" => エラー
