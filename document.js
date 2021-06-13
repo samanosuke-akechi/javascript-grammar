78396896ff188f9d0fb1c4bebff91e9f17371b6e
samanosuke-akechi <78411965+samanosuke-akechi@users.noreply.github.com>
2021-06-13 22:45:08 +0900

    documentオブジェクトについてまとめるファイル生成
    document.jsにdocumentオブジェクトについてまとめる予定。
    htmlファイルにdocument.jsを読み込む記述をした。

---
+++ document.js
@@ -0,0 +1,7 @@
+// documentオブジェクト
+
+// getElementByIdメソッド
+  // 引数に指定したidのHTMLの要素を取得する
+const get_p = document.getElementById("test");  // testというidを持ったHTMLの要素を取得
+console.log(get_p);
+get_p.innerHTML = "Hello World!!"
