d4e01f4225a5b39551d0c7cec997cb80fc95eed8
samanosuke-akechi <78411965+samanosuke-akechi@users.noreply.github.com>
2021-06-24 11:55:48 +0900

    即時関数とアロー関数追加

--- function.js
+++ function.js
@@ -20,7 +20,22 @@ function calc(num1, num2) {
 console.log(calc(3, 4));  // 出力結果：12 （returnがないとundefinedが出力される）
 
 // 関数式
-const callAnimal = function(animal) {
+const callAnimal = function(animal) {  // function(){}の部分を無名関数という
   console.log(animal);
 }
 callAnimal("cat");
+
+// 関数式と関数宣言の違い
+  // 関数式は定義してからでないと呼べない（読み込みは定義した後である必要がある）
+  // 関数宣言の読み込みが先にきても読み込むことができる
+
+// 即時関数
+(function countNum(num) {
+  console.log(num);
+})(1)  // この時点で呼び出している → 出力結果：1
+
+// アロー関数
+const callName = (name1) => {
+  console.log(name1);
+}
+callName("田中");
