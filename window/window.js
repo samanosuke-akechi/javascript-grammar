cc503b3cf70fdcea653b7dc62773ca33283a97e3
samanosuke-akechi <78411965+samanosuke-akechi@users.noreply.github.com>
2021-06-28 23:10:41 +0900

    window.openとwindow.close

--- window/window.js
+++ window/window.js
@@ -6,6 +6,8 @@ alert("windowオブジェクトはwindowの記述を省略できる")
 
 const button1 = document.getElementById("button1");
 const button2 = document.getElementById("button2");
+const button3 = document.getElementById("button3");
+const button4 = document.getElementById("button4");
 
 const alert1 = () => {
   window.alert("ボタン1をクリック")
@@ -15,3 +17,15 @@ const alert2 = () => {
 }
 button1.addEventListener('click', alert1);
 button2.addEventListener('click', alert2);
+
+// window.openとwindow.close
+let google
+const openGoogle = () => {
+  google = window.open('https://www.google.com/')
+}
+const closeGoogle = () => {
+  google.close()
+}
+
+button3.addEventListener('click', openGoogle);
+button4.addEventListener('click', closeGoogle);
