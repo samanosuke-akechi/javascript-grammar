c756a7088613a22a615832d159721bad7ed9ed76
samanosuke-akechi <78411965+samanosuke-akechi@users.noreply.github.com>
2021-06-29 19:37:09 +0900

    documentフォルダ作成

--- document.js
+++
@@ -1,7 +0,0 @@
-// documentオブジェクト
-
-// getElementByIdメソッド
-  // 引数に指定したidのHTMLの要素を取得する
-const get_p = document.getElementById("test");  // testというidを持ったHTMLの要素を取得
-console.log(get_p);
-get_p.innerHTML = "Hello World!!"
---
+++ document/document.js
@@ -0,0 +1,15 @@
+// documentオブジェクト
+
+// getElementById("id名")
+  // 引数に指定したidのHTMLの要素を取得する
+const get_p = document.getElementById("test");  // testというidを持ったHTMLの要素を取得
+get_p.innerHTML = "Hello World!!"  //innerHTMLで要素の書き換え
+console.log(get_p);  // 出力結果：<p id="test">Hello World!!</p>
+console.log(get_p.innerHTML);  // 出力結果：Hello World!!
+
+// document.getElementsByClassName("クラス名")
+  // 引数に指定したクラス名のHTML要素を取得する
+const getClass = document.getElementsByClassName("hoge");  // hogeというクラス名を持ったHTMLの要素を取得
+console.log(getClass);  // 出力結果：HTMLCollection(2) [div.hoge, h1.hoge]
+getClass[1].innerHTML = "h1要素"
+console.log(getClass[1]);  // 出力結果：<h1 class="hoge">h1要素</h1>
